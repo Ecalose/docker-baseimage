da15dbac778d8c0797169a88da04f3d23f846271
Jocelyn Le Sage <jocelyn@le-sage.com>
2026-04-02 18:34:05 -0400

    Moved internal variables file to `/tmp/` (it doesn't need to persist across "reboot").

--- rootfs/etc/cont-init.d/10-clean-tmp-dir.sh
+++ rootfs/etc/cont-init.d/10-clean-tmp-dir.sh
@@ -6,6 +6,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-rm -rf /tmp/* /tmp/.[!.]*
+find /tmp -mindepth 1 -maxdepth 1 ! -name '.cont-env-internal' -exec rm -rf {} +
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
